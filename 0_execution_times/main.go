8a96c3c1aa22f3548c5f752fb3a0d671626a277a
drei <andredbenedetti@gmail.com>
2023-09-28 16:25:24 -0300

    wip

--- 0_execution_times/main.go
+++ 0_execution_times/main.go
@@ -7,75 +7,74 @@ import (
 )
 
 type Timeframe struct {
-    duration time.Duration
-    label string
+	duration time.Duration
+	label    string
 }
-
 
 type Fn struct {
-    label string
-    fn func(n int) float64
+	label string
+	fn    func(n float64) float64
 }
 
-
-
-
+// Something is fundamentally wrong with this code, just don't know what yet
 func main() {
-    // Given the timeframes: second, minute, hour, day, month, year, century
+	fns := []Fn{
+		// {"n", func(n float64) float64 { return float64(n)}},
+		{"lg(n)", func(n float64) float64 { return math.Log2(float64(n)) }},
+		{"sqrt(n)", func(n float64) float64 { return math.Sqrt(float64(n)) }},
+		// and so on
+		// ...
+	}
 
-    // determine biggest size 'n' of problem that can be solved in each
-    // time frame, considering that each run takes f(n) microseconds
+	// The largest representable Duration is approximately
+	// 290 years, according to the golang docs.
+	// Thus, one we're good representing a century as a Duration.
+	const fnDuration = time.Microsecond
+	// timeframes := [7]Timeframe{
+	// 	{time.Second, "second"},
+	// 	{time.Minute, "minute"},
+	// 	{time.Hour, "hour"},
+	// 	{time.Hour * 24, "day"},
+	// 	{time.Hour * 24 * 30, "month"},
+	// 	{time.Hour * 24 * 30 * 12, "year"},
+	// 	{time.Hour * 24 * 30 * 12 * 100, "century"},
+	// }
 
-    // f(n):
-    // lg(n)
-    // sqrt(n)
-    // n
-    // n * lg(n)
-    // n^2
-    // n^3
-    // 2^n
-    // n!
-
-    fns := []Fn{
-        {"n", func(n int) float64 { return float64(n)}},
-        {"lg(n)", func(n int) float64 { return math.Log2(float64(n))}},
-        {"sqrt(n)", func(n int) float64 { return math.Sqrt(float64(n))}},
-        // and so on
-        // ...
-    }
-
-    // We want to know:
-    // (timeframe_in_microseconds) / f(n) = 1, find n
-
-    // for example, if f(n) = n and timeframe = 1 second
-    //  1000000 / n = 1
-    //  n = 1000000
-
-    // if timeframe = 1 minute,
-    // 1000000 * 60 / n = 1
-    // n = 60000000
-
-    // notice that the largest representable Duration is
-    // approximately 290 years, according to the golang docs.
-    // Thus, a century is still representable like this.
-    const fnDuration = time.Microsecond
-    timeframes := [7]Timeframe{
-        {time.Second,"second"},
-        {time.Minute,"minute"},
-        {time.Hour,"hour"},
-        {time.Hour * 24, "day"},
-        {time.Hour * 24 * 30, "month"},
-        {time.Hour * 24 * 30 * 12,"year"},
-        {time.Hour * 24 * 30 * 12 * 100, "century"},
-    }
-
-    for _, tf := range(timeframes) {
-        fmt.Printf("Timeframe: %s\n", tf.label)
-        // TODO: computeOps()
-    }
+	// Turns out we can't compute for a second in the way this was designed
+	// as the max number of operations gets too big.
+	// We'll compute it for 50 microseconds and extrapolate it for the other timeframes.
+	baseTimeframe := Timeframe{label: "ms", duration: 50 * time.Microsecond}
+	for _, fn := range fns {
+		fmt.Printf("Computing for function %s\n", fn.label)
+		guess := 1000.00
+		newtonMethodIterations := 6
+		for i := 0; i < newtonMethodIterations; i++ {
+			guess = IterateNewtonsMethod(fn, guess, time.Microsecond, baseTimeframe)
+		}
+		fmt.Printf("Max operations: %f\n", guess)
+	}
 }
 
+func evaluatePoint(fn Fn, n float64, opDuration time.Duration, tf Timeframe) float64 {
+	return fn.fn(n)*float64(opDuration.Microseconds()) - float64(tf.duration.Microseconds())
+}
 
+// Return a new guess based on a previous guess
+// We're using a simplified Newton's method, where
+// instead of the derivative of the fn, we use the
+func IterateNewtonsMethod(fn Fn, guess float64, opDuration time.Duration, tf Timeframe) float64 {
+	// we have g(n) = fn(n) * duration - timeframe = 0
+
+	// rough approximation to the derivative at the point
+	// using two nearby points separated by 'step'
+	step := guess / 100
+	x1 := guess + step
+	x0 := guess - step
+	dn := (evaluatePoint(fn, x1, opDuration, tf) - evaluatePoint(fn, x0, opDuration, tf)) / (x1 - x0)
+
+	return guess - evaluatePoint(fn, guess, opDuration, tf)/dn
+
+}
 
 // Probably need a numeric method here
 // to generalize for any function
