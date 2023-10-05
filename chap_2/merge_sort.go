6140194ed37555cacb7504fb796a448088b1fecf
drei <andredbenedetti@gmail.com>
2023-10-05 20:36:14 -0300

    implement Merge

--- chap_2/merge_sort.go
+++ chap_2/merge_sort.go
@@ -2,11 +2,31 @@ package chap2
 
 import "fmt"
 
-/* Assumes the slice of the input bounded
-by start, middle and end are locally sorted */
+// Assumes the slice of the input bounded
+// by start, middle and end are locally sorted */
+func Merge(i *[]int, start int, middle int, end int) {
+	left := make([]int, middle-start)
+	copy(left, (*i)[:middle])
+	right := make([]int, end-middle)
+	copy(right, (*i)[middle:end])
 
-func Merge(input []int, start int, middle int, end int) {
-
+	l := 0
+	r := 0
+	for l < len(left) || r < len(right) {
+		if l >= len(left) {
+			(*i)[start+l+r] = right[r]
+			r++
+		} else if r >= len(right) {
+			(*i)[start+l+r] = left[l]
+			l++
+		} else if left[l] < right[r] {
+			(*i)[start+l+r] = left[l]
+			l++
+		} else {
+			(*i)[start+l+r] = right[r]
+			r++
+		}
+	}
 }
 
 // 'start' is included, 'end' is excluded
@@ -17,6 +37,9 @@ func MergeSort(input []int, start int, end int) {
 		fmt.Printf("Merging with start %d and end %d\n", start, end)
 		// Split in half
 		// middle is rounded down
+
+		// 0 (inclusive) + 4 (not inclusive) / 2 = 2
+		//
 		middle := (start + end) / 2
 		fmt.Printf("Middle %d\n", middle)
 		MergeSort(input, start, middle)
